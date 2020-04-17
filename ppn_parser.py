c39ded51403eb8e7b8726492244d5557500ce0ab
Ashafix <maximili.peters@mail.huji.ac.il>
2020-04-17 15:25:45 +0200

    more robust parsing

--- ppn_parser.py
+++ ppn_parser.py
@@ -1,49 +1,32 @@
 import collections
 import xml.etree.cElementTree as ET
 
-
+VALID_DELIMITERS = set(('\\u001d', '\\x1D', '\\x1d', '\u001d', '\x1d'))
 Ppn = collections.namedtuple('PPN', ['pzn', 'lot', 'exp', 'sn'])
 
 
 def parse_ppn(ppn, to_xml=False):
-    def parse_cell(cell):
-        application_identifier = cell[0:2]
-        if application_identifier == '01':
-            prefix = cell[3:7]
-            if prefix == '4150':
-                pzn = cell[7:15]
-                check_digit = int(cell[15])
-                check_sum = sum([int(x) * (weight + 1) for weight, x in enumerate(pzn)])
-                calculated_check_digit = check_sum % 11
-                if calculated_check_digit != check_digit:
-                    raise ValueError('invalid check digit, expected {}, got {}'.format(check_digit,
-                                                                                       calculated_check_digit))
-                return cell[16:], pzn
-            else:
-                raise ValueError('unknown prefix: {}'.format(prefix))
-        if application_identifier == '10':
-            lot_number = cell[2:]
-            return '', lot_number
-        if application_identifier == '17':
-            exp_date = cell[2:8]
-            return cell[8:], exp_date
-        if application_identifier == '21':
-            serial_number = cell[2:]
-            return '', serial_number
-        raise ValueError('unknown application identifier: {}'.format(application_identifier))
+
     while ppn.startswith('"') or ppn.startswith("'"):
         ppn = ppn[1:]
     while ppn.endswith('"') or ppn.endswith("'"):
         ppn = ppn[:-1]
-    cells = ppn.split('\\u001d')
-    if len(cells) != 3:
+
+    if not is_valid_ppn(ppn):
         raise ValueError('invalid PPN')
-    cells = cells[1:]
-    cell, pzn = parse_cell(cells[0])
-    _, lot_number = parse_cell(cell)
-    cell, exp_date = parse_cell(cells[1])
-    _, serial_number = parse_cell(cell)
-    ppn = Ppn(pzn, lot_number, exp_date, serial_number)
+    for valid_delimiter in VALID_DELIMITERS:
+        if valid_delimiter in ppn:
+            cells = ppn.split(valid_delimiter)[1:]
+            break
+
+    parsed = {}
+    for cell in cells:
+        while len(cell) > 0:
+            cell, key, value = _parse_cell(cell)
+            if key:
+                parsed[key] = value
+
+    ppn = Ppn(parsed.get('pzn', ''), parsed.get('lot', ''), parsed.get('exp', ''), parsed.get('sn', ''))
     if to_xml:
         return ppn_to_xml(ppn)
     return ppn
@@ -57,3 +40,50 @@ def ppn_to_xml(ppn):
         ET.SubElement(doc, k.upper()).text = v
 
     return ET.tostring(doc).decode()
+
+
+def is_valid_ppn(ppn, valid_delimiters=None):
+    if valid_delimiters is None:
+        valid_delimiters = VALID_DELIMITERS
+    counter = 0
+    for valid_delimiter in valid_delimiters:
+        if ppn.count(valid_delimiter) == 0:
+            continue
+        if ppn.count(valid_delimiter) in (1, 2):
+            counter += 1
+        else:
+            return False
+
+    return counter == 1
+
+
+def _parse_cell(cell):
+    application_identifier = cell[0:2]
+    if application_identifier == '01':
+        if len(cell) < 15:
+            raise ValueError('information too short, expected at least 15 characters, got: {}'.format(len(cell)))
+        prefix = cell[3:7]
+        if prefix == '4150':
+            pzn = cell[7:14]
+            check_digit = int(cell[14])
+            check_sum_pzn = sum([int(x) * (weight + 1) for weight, x in enumerate(pzn)])
+            calculated_check_digit = check_sum_pzn % 11
+            if calculated_check_digit != check_digit:
+                raise ValueError('invalid check digit for PNZ, expected {}, got {}'.format(check_digit,
+                                                                                           calculated_check_digit))
+            # TODO add NTIN check
+            return cell[16:], 'pzn', pzn
+        else:
+            raise ValueError('unknown prefix: {}'.format(prefix))
+    if application_identifier == '10':
+        lot_number = cell[2:]
+        return '', 'lot', lot_number
+    if application_identifier == '17':
+        exp_date = cell[2:8]
+        return cell[8:], 'exp', exp_date
+    if application_identifier == '21':
+        serial_number = cell[2:]
+        return '', 'sn', serial_number
+    if application_identifier == '':
+        return '', '', ''
+    raise ValueError('unknown application identifier: {}'.format(application_identifier))
